df6ffcc3bd7cd97cbf55b2a1dd8cabcad9c34bb7
Siraj M <sirajmsaha@hotmail.com>
2018-10-28 15:15:40 +0400

    Add project files.

---
+++ ObjectBinding/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ObjectBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dpDob = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.bindingObj = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingObj)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(147, 63);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(180, 29);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Location = new System.Drawing.Point(147, 172);
+            this.cbGender.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(180, 29);
+            this.cbGender.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(147, 102);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(180, 29);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(143, 136);
+            this.lblFullName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(24, 21);
+            this.lblFullName.TabIndex = 0;
+            this.lblFullName.Text = "??";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 175);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Gender:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 215);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Gender:";
+            // 
+            // dpDob
+            // 
+            this.dpDob.CustomFormat = "dd/mm/yy";
+            this.dpDob.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dpDob.Location = new System.Drawing.Point(147, 209);
+            this.dpDob.Name = "dpDob";
+            this.dpDob.Size = new System.Drawing.Size(180, 29);
+            this.dpDob.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(252, 260);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 464);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dpDob);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Object Binding Form ";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingObj)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dpDob;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.BindingSource bindingObj;
+    }
+}
+
---
+++ ObjectBinding/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectBinding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            cbGender.DataSource = Enum.GetValues(typeof(Gender));
+            bindingObj.DataSource = new List<User>();
+            AddNew();
+        }
+
+        private void AddNew()
+        {
+            bindingObj.AddNew();
+
+            txtFirstName.DataBindings.Clear();
+            txtLastName.DataBindings.Clear();
+            cbGender.DataBindings.Clear();
+            dpDob.DataBindings.Clear();
+            lblFullName.DataBindings.Clear();
+            btnSave.DataBindings.Clear();
+
+            txtFirstName.DataBindings.Add("Text", bindingObj, "FirstName");
+            txtLastName.DataBindings.Add("Text", bindingObj, "LastName");
+            cbGender.DataBindings.Add("SelectedItem", bindingObj, "Gender");
+            dpDob.DataBindings.Add("Value", bindingObj, "DateOfBirth");
+            lblFullName.DataBindings.Add("Text", bindingObj, "FullName");
+
+            btnSave.DataBindings.Add("Enabled", bindingObj, "IsValid");
+        }
+
+
+        private void Save()
+        {
+            User _current = (User)bindingObj.Current;
+            if (_current != null)
+            {
+                if (_current.IsValid)
+                {
+                    bindingObj.Add(_current);
+                    AddNew();
+                }
+                else
+                {
+                    MessageBox.Show("First name and last name is required");
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+    }
+
+    public enum Gender
+    {
+        Male=0,
+        Female=1
+    }
+
+    public class User
+    {
+        public User()
+        {
+            DateOfBirth = DateTime.Now;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+
+
+        public string FullName => $"{this.FirstName} {this.LastName}".Trim();
+        public bool IsAgeValid => DateOfBirth.Year <= DateTime.Now.AddYears(-18).Year;
+        public bool IsValid => !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName) && IsAgeValid; 
+    }
+
+}
